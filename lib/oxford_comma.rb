1197a13decf722f8b597fbda4ffc8ee3f8efbaa8
Martin Moy <martinmoy1991@gmail.com>
2020-07-14 00:36:54 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -2,10 +2,10 @@ def oxford_comma(array)
   if array.length == 1
     array.join
   elsif array.length == 2
-      return array.join(" and ")
+       array.join(" and ")
     else array.length >= 3
       array[-1] = "and #{array[-1]}"
-      return array.join(", ")
+     array.join(", ")
     end
 
 end
